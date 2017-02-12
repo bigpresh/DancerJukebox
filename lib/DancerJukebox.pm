272d01fc1115eea6e8cb8258faebe68d81b93435
David Precious <davidp@preshweb.co.uk>
2017-02-12 21:33:03 +0000

    Factor out finding playing next song from queue.
    Want to use this from the "skip" route so skipping plays a song from the queue
    rather than just something else at random.

--- lib/DancerJukebox.pm
+++ lib/DancerJukebox.pm
@@ -161,4 +161,56 @@ any '/enabled' => sub {
 };
 
 
+sub play_queued_song {
+    my $song = shift;
+
+    # OK - do our magic!
+    debug("OK, about to add $song->{path}");
+    mpd->playlist->add($song->{path});
+    debug("Added $song->{path} to playlist");
+    # It should be a pretty safe bet that it'll have been added at the
+    # end, so find out how many tracks are now on the playlist, and
+    # jump to the last:
+    debug("Playlist is " . mpd->status->playlistlength
+        . " items long, so playing that -1");
+    mpd->play( mpd->status->playlistlength -1 );
+    mark_played($song->{id});
+}
+
+
+{
+    my $fetch_next_sth;
+    sub next_in_queue {
+        # Annoyingly, the syntax to randomly select a row varies depending on
+        # the DB engine:
+        my %order_by_rand = (
+            mysql => 'ORDER BY RAND()',
+            sqlite => 'ORDER BY RANDOM()',
+        );
+        my $order_by;
+        if (config->{random}) {
+            $order_by = $order_by_rand{ lc database->{Driver}{Name} }
+                or warn "Don't know how to get random rows with this"
+                    . " database engine!";
+        } else {
+            $order_by = 'queued ASC';
+        }
+        $fetch_next_sth ||= database->prepare(
+            "select * from queue where played is null $order_by limit 1");
+        $fetch_next_sth->execute()
+            or warn "Failed to execute query - " . database->errstr;
+        return $fetch_next_sth->fetchrow_hashref;
+    }
+    my $delete_sth;
+    sub mark_played {
+        my $datetime = DateTime->now;
+        my $datestamp = join ' ', $datetime->ymd, $datetime->hms;
+        $delete_sth ||= 
+            database->prepare('update queue set played = ? where id = ?');
+        $delete_sth->execute($datestamp, shift)
+            or warn "Failed to execute query - " . database->errstr;
+        return 1;
+    }
+}
+
 true;
